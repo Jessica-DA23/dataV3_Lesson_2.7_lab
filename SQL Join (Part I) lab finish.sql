f9b08a4ecb9d1c9e47c6764fbdd05fd5f4caa979
Jessica <jessicamargaridacastelo@hotmail.com>
2023-10-26 18:52:32 +0100

    SQL Join (Part I) lab finish

--- SQL Join (Part I) lab finish.sql
+++ SQL Join (Part I) lab finish.sql
@@ -15,17 +15,14 @@ JOIN film f ON fc.film_id = f.film_id
 GROUP BY c.name;
 
 #2. Display the total amount rung up by each staff member in August of 2005.
-SELECT *
-FROM payment;
-
 SELECT s.staff_id, SUM(p.amount) AS total_amount, CONCAT(s.first_name, ' ', s.last_name) AS staff_name
 FROM staff s
 JOIN payment p ON s.staff_id = p.staff_id
 WHERE YEAR(p.payment_date) = 2005 AND MONTH(p.payment_date) = 8
 GROUP BY s.staff_id, staff_name;
 
-#3. Which actor has appeared in the most films?
 
+#3. Which actor has appeared in the most films?
 SELECT CONCAT(a.first_name, ' ', a.last_name) AS actor_name, a.actor_id, COUNT(fa.actor_id) AS 'film_count'
 FROM sakila.actor AS a
 JOIN sakila.film_actor AS fa
@@ -34,6 +31,7 @@ GROUP BY a.actor_id, a.first_name, a.last_name
 ORDER BY film_count DESC
 LIMIT 1;
 
+
 #4. Most active customer (the customer that has rented the most number of films)
 SELECT c.first_name, c.last_name, COUNT(r.rental_id) 
 FROM customer c
@@ -41,7 +39,6 @@ JOIN rental r ON c.customer_id = r.customer_id
 GROUP BY c.first_name, c.last_name
 ORDER BY COUNT(r.rental_id)  DESC
 LIMIT 1;
-
 
 
 #5. Display the first and last names, as well as the address, of each staff member.
@@ -57,15 +54,13 @@ LEFT JOIN film_actor fa ON f.film_id = fa.film_id
 GROUP BY f.title;
 
 
-
-
 #7. Using the tables `payment` and `customer` and the JOIN command, list the total paid by each customer.
 #List the customers alphabetically by last name.
-SELECT c.customer_id,  CONCAT(c.first_name, ' ', c.last_name) AS customer_name, SUM(p.amount) AS total_paid
+SELECT c.customer_id,  CONCAT(c.last_name, ' ', c.first_name) AS customer_name, SUM(p.amount) AS total_paid
 FROM customer c
 JOIN payment p ON c.customer_id = p.customer_id
-GROUP BY c.customer_id, customer_name
-ORDER BY customer_name;
+GROUP BY last_name
+ORDER BY last_name;
 
 
 #8. List the titles of films per `category`.
